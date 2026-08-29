11b0f1768e7c4d4bcce4523b29a017147afad120
prc33 <pete@skiresults.co.uk>
2026-08-29 05:32:50 +0000

    keep CType encoding with type interfaces

--- compiler/tcc.h
+++ compiler/tcc.h
@@ -717,65 +717,6 @@ struct filespec {
 #define VT_SYM       0x0200  /* a symbol value is added */
 #define VT_MUSTCAST  0x0C00  /* value must be casted to be correct (used for
                                 char/short stored in integer registers) */
-/* types */
-#define VT_BTYPE       0x000f  /* mask for basic type */
-#define VT_VOID             0  /* void type */
-#define VT_BYTE             1  /* signed byte type */
-#define VT_SHORT            2  /* short type */
-#define VT_INT              3  /* integer type */
-#define VT_LLONG            4  /* 64 bit integer */
-#define VT_PTR              5  /* pointer */
-#define VT_FUNC             6  /* function type */
-#define VT_STRUCT           7  /* struct/union definition */
-#define VT_FLOAT            8  /* IEEE float */
-#define VT_DOUBLE           9  /* IEEE double */
-#define VT_LDOUBLE         10  /* IEEE long double */
-#define VT_BOOL            11  /* ISOC99 boolean type */
-#define VT_QLONG           13  /* 128-bit integer. Only used for x86-64 ABI */
-#define VT_QFLOAT          14  /* 128-bit float. Only used for x86-64 ABI */
-
-#define VT_UNSIGNED    0x0010  /* unsigned type */
-#define VT_DEFSIGN     0x0020  /* explicitly signed or unsigned */
-#define VT_ARRAY       0x0040  /* array type (also has VT_PTR) */
-#define VT_BITFIELD    0x0080  /* bitfield modifier */
-#define VT_CONSTANT    0x0100  /* const modifier */
-#define VT_VOLATILE    0x0200  /* volatile modifier */
-#define VT_VLA         0x0400  /* VLA type (also has VT_PTR and VT_ARRAY) */
-#define VT_LONG        0x0800  /* long type (also has VT_INT rsp. VT_LLONG) */
-
-/* storage */
-#define VT_EXTERN  0x00001000  /* extern definition */
-#define VT_STATIC  0x00002000  /* static variable */
-#define VT_TYPEDEF 0x00004000  /* typedef definition */
-#define VT_INLINE  0x00008000  /* inline definition */
-/* currently unused: 0x000[1248]0000  */
-
-#define VT_STRUCT_SHIFT 20     /* shift for bitfield shift values (32 - 2*6) */
-#define VT_STRUCT_MASK (((1U << (6+6)) - 1) << VT_STRUCT_SHIFT | VT_BITFIELD)
-#define BIT_POS(t) (((t) >> VT_STRUCT_SHIFT) & 0x3f)
-#define BIT_SIZE(t) (((t) >> (VT_STRUCT_SHIFT + 6)) & 0x3f)
-
-#define VT_UNION    (1 << VT_STRUCT_SHIFT | VT_STRUCT)
-#define VT_ENUM     (2 << VT_STRUCT_SHIFT) /* integral type is an enum really */
-#define VT_ENUM_VAL (3 << VT_STRUCT_SHIFT) /* integral type is an enum constant really */
-
-#define IS_ENUM(t) ((t & VT_STRUCT_MASK) == VT_ENUM)
-#define IS_ENUM_VAL(t) ((t & VT_STRUCT_MASK) == VT_ENUM_VAL)
-#define IS_UNION(t) ((t & (VT_STRUCT_MASK|VT_BTYPE)) == VT_UNION)
-
-/* type mask (except storage) */
-#define VT_STORAGE (VT_EXTERN | VT_STATIC | VT_TYPEDEF | VT_INLINE)
-#define VT_TYPE (~(VT_STORAGE|VT_STRUCT_MASK))
-
-/* symbol was created by tccasm.c first */
-#define VT_ASM (VT_VOID | VT_UNSIGNED)
-#define IS_ASM_SYM(sym) (((sym)->type.t & (VT_BTYPE | VT_ASM)) == VT_ASM)
-
-/* general: set/get the pseudo-bitfield value for bit-mask M */
-#define BFVAL(M,N) ((unsigned)((M) & ~((M) << 1)) * (N))
-#define BFGET(X,M) (((X) & (M)) / BFVAL(M,1))
-#define BFSET(X,M,N) ((X) = ((X) & ~(M)) | BFVAL(M,N))
-
 /* token values */
 
 /* conditional ops */
--- compiler/types.h
+++ compiler/types.h
@@ -1,5 +1,60 @@
 #ifndef TCC_TYPES_H
 #define TCC_TYPES_H
+
+/* CType.t: basic type, qualifiers, storage, and bitfield metadata. */
+#define VT_BTYPE       0x000f
+#define VT_VOID             0
+#define VT_BYTE             1
+#define VT_SHORT            2
+#define VT_INT              3
+#define VT_LLONG            4
+#define VT_PTR              5
+#define VT_FUNC             6
+#define VT_STRUCT           7
+#define VT_FLOAT            8
+#define VT_DOUBLE           9
+#define VT_LDOUBLE         10
+#define VT_BOOL            11
+#define VT_QLONG           13
+#define VT_QFLOAT          14
+
+#define VT_UNSIGNED    0x0010
+#define VT_DEFSIGN     0x0020
+#define VT_ARRAY       0x0040
+#define VT_BITFIELD    0x0080
+#define VT_CONSTANT    0x0100
+#define VT_VOLATILE    0x0200
+#define VT_VLA         0x0400
+#define VT_LONG        0x0800
+
+#define VT_EXTERN  0x00001000
+#define VT_STATIC  0x00002000
+#define VT_TYPEDEF 0x00004000
+#define VT_INLINE  0x00008000
+
+#define VT_STRUCT_SHIFT 20
+#define VT_STRUCT_MASK (((1U << 12) - 1) << VT_STRUCT_SHIFT | VT_BITFIELD)
+#define BIT_POS(t) (((t) >> VT_STRUCT_SHIFT) & 0x3f)
+#define BIT_SIZE(t) (((t) >> (VT_STRUCT_SHIFT + 6)) & 0x3f)
+
+#define VT_UNION    (1 << VT_STRUCT_SHIFT | VT_STRUCT)
+#define VT_ENUM     (2 << VT_STRUCT_SHIFT)
+#define VT_ENUM_VAL (3 << VT_STRUCT_SHIFT)
+
+#define IS_ENUM(t) ((t & VT_STRUCT_MASK) == VT_ENUM)
+#define IS_ENUM_VAL(t) ((t & VT_STRUCT_MASK) == VT_ENUM_VAL)
+#define IS_UNION(t) ((t & (VT_STRUCT_MASK | VT_BTYPE)) == VT_UNION)
+#define VT_STORAGE (VT_EXTERN | VT_STATIC | VT_TYPEDEF | VT_INLINE)
+#define VT_TYPE (~(VT_STORAGE | VT_STRUCT_MASK))
+
+/* Symbol created by the assembler before C parsing sees it. */
+#define VT_ASM (VT_VOID | VT_UNSIGNED)
+#define IS_ASM_SYM(sym) (((sym)->type.t & (VT_BTYPE | VT_ASM)) == VT_ASM)
+
+#define BFVAL(M, N) ((unsigned)((M) & ~((M) << 1)) * (N))
+#define BFGET(X, M) (((X) & (M)) / BFVAL(M, 1))
+#define BFSET(X, M, N) ((X) = ((X) & ~(M)) | BFVAL(M, N))
+
 ST_INLN int is_float(int t);
 ST_FUNC int is_integer_btype(int bt);
 ST_FUNC int btype_size(int bt);
